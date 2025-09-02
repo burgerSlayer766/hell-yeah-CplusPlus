b23b1d2e0c192c02abd0b2490ff00386ee5b6b17
burger slayer <burgerslayer77@gmail.com>
2025-09-02 16:36:14 +0300

    laboratory work

---
+++ 1Lb Cplus.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+	setlocale(LC_ALL, "Russian");
+
+	double x;
+
+	double y;
+
+	cout << ("ВВЕДИТЕ ЧИСлО ");
+
+	cin >> x;
+
+	cout << ("ВВЕДИТЕ ЧИСЛО ");
+
+	cin >> y;
+
+
+	double c = pow((y * pow(x, 2) - 1), 2) / 2 * (pow(cos(y), 2) - sin(pow(x, 2)));
+
+	cout << c;
+}
